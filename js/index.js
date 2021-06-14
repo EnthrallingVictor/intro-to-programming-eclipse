359ae63a77a99db0ef16e8d7d4daa8b391c229dd
EnthrallingVictor <victor.v.1991@gmail.com>
2021-06-13 22:35:32 -0400

    lesson 4-2

--- js/index.js
+++ js/index.js
@@ -8,5 +8,17 @@ const copyright = document.createElement('p')
 
 copyright.innerHTML = `Victor Valdez ${thisYear}`
 
+footer.appendChild(copyright)
 
+const skills = [`Programming`, `HTML`, `Javascript`, `CSS`, `Customer Support`, `Spanish`]
+
+const skillsSection = document.querySelector('#skills')
+
+let skillsList = skillsSection.querySelector('ul')
+
+for (let i=0; i<skills.length; i++) {
+    let skill = document.createElement('li')
+    skill.innerHTML = skills[i]
+    skillsList.appendChild(skill)
+}
 
